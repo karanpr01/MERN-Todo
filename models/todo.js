3b9c691674b2e43922b36257b7aa7384efce9f2e
Prem <karanprem01@gmail.com>
2025-11-20 13:06:50 +0530

    Create userModel and TodoModel

---
+++ models/todo.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "in-progress", "completed"],
+      default: "pending",
+    },
+    dueOn: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+export const Todo = mongoose.model("Todo", todoSchema);
---
+++ models/user.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+    
+
+const userSchema = new mongoose.Schema({
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true, // This enforces uniqueness
+            trim: true, 
+        },
+        password: {
+            type: String,
+            required: true
+        }
+});
+
+   
+
+export const User = mongoose.model('User', userSchema);
